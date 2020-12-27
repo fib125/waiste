b1feed39a992364594b450efc79170541798ed4f
fib125 <mitch.coolican@gmail.com>
2020-12-27 10:47:49 -0600

    added module search-home to the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchHomeModule } from './search-home/search-home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SearchHomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
